b1765ea42db5b2a92be90dd9675917de046e5440
asA-win <asA-win>
2025-04-07 23:04:55 +0530

    changes

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+def home(request):
+    return HttpResponse("Hello, world!")
 
 urlpatterns = [
+    path('',home),
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
